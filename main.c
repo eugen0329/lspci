d5c853d6a1178f9b8ae4002cd455eb5243450b4d
eugen0329 <eugen0329@gmail.com>
2015-04-14 18:27:32 +0000

    Added a wider pci_codes.h, fixed cycle by adding pass by functions

--- main.c
+++ main.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <sys/io.h>
 
 #include "pci_codes.h"
+
 
 #define ALL_IO    3
 #define RVAL_ERR -1
@@ -15,11 +19,14 @@
 #define CONFIG_DATA    0x0CFC
 #define PCI_MAX_BUS_COUNT ( 1 << 8 )
 #define PCI_MAX_DEV_COUNT ( 1 << 5 )
+#define PCI_MAX_FU_COUNT  ( 1 << 3 )
 
 #define SET_BUS( bus ) ( bus << 16 )
 #define SET_DEV( dev ) ( dev << 11 )
+#define SET_FU( fu )   ( fu << 8 )
 
-#define GET_REG_ADDR( bus, dev, offs ) ( (1 << 31) | SET_BUS(bus) | SET_DEV(dev) | offs )
+#define GET_REG_ADDR( bus, dev, fu, offs ) \
+    ( (1 << 31) | SET_BUS(bus) | SET_DEV(dev) | SET_FU(fu) | offs )
 #define GET_VENDOR_ID( dat )        ( (dat >> 0)  & 0xFFFF )
 #define GET_DEV_ID( dat )           ( (dat >> 16) & 0xFFFF )
 #define GET_CLASS_BASE( dat )       ( (dat >> (8 + 16)) & 0xFF )
@@ -57,13 +64,13 @@ classInfo_t getClassInfo(classCode_t* cl);
 
 int main(int argc, char *argv[])
 {
-    uint8_t  dev, maxDevsCount = PCI_MAX_DEV_COUNT;
+    const char * venName;
+    uint8_t  dev, maxDevsCount = PCI_MAX_DEV_COUNT, maxFuCount = PCI_MAX_FU_COUNT;
     uint16_t bus, maxBusCount  = PCI_MAX_BUS_COUNT;
     uint16_t venId, devId;
-    uint8_t  rev;
-    uint32_t regdat;
+    uint8_t  rev, fu;
     uint16_t svenId;
-    const char * venName;
+    uint32_t regdat;
 
     classCode_t cl;
     devInfo_t devinf;
@@ -71,37 +78,37 @@ int main(int argc, char *argv[])
 
     if(increasePrivelegies(ALL_IO)) exit(EXIT_FAILURE);
 
-    puts(" bus,dev | vendor | dev id | class b,s,i | rev  | svenId | descriptions ...");
-    puts("---------+--------+--------+-------------+------+--------+-----------------");
+    puts("bus,dev,f|vendor| dev | class bsi|rev|svenId| descriptions ...");
+    puts("---------+------+-----+----------+---+------+-----------------");
 
     for(bus = 0; bus < maxBusCount; bus++) {
         for (dev = 0; dev < maxDevsCount; dev++) {
-            outl(GET_REG_ADDR(bus, dev, 0x0), CONFIG_ADDRESS);
-            regdat = inl(CONFIG_DATA);
+            for(fu = 0; fu < maxFuCount; fu++) {
+                outl(GET_REG_ADDR(bus, dev, fu, 0x0), CONFIG_ADDRESS);
+                regdat = inl(CONFIG_DATA);
 
-            if(regdat == NULL_DEV_REG_DATA) continue;
+                if(regdat == NULL_DEV_REG_DATA) continue;
 
-            venId = GET_VENDOR_ID(regdat);
-            devId = GET_DEV_ID(regdat);
+                venId = GET_VENDOR_ID(regdat);
+                devId = GET_DEV_ID(regdat);
 
-            outl(GET_REG_ADDR(bus, dev, 0x8), CONFIG_ADDRESS);
-            regdat = inl(CONFIG_DATA);
-            rev = GET_REVISION_ID(regdat);
+                outl(GET_REG_ADDR(bus, dev, fu, 0x8), CONFIG_ADDRESS);
+                regdat = inl(CONFIG_DATA);
+                rev = GET_REVISION_ID(regdat);
+                GET_CLASS_CODE(cl);
 
-            GET_CLASS_CODE(cl);
+                outl(GET_REG_ADDR(bus, dev, fu, 0x2C), CONFIG_ADDRESS);
+                svenId = GET_SUBSYS_VENDOR_ID(inl(CONFIG_DATA));
+                venName = getVenName(venId);
+                devinf = getDevInfo(devId, venId);
+                clInf = getClassInfo(&cl);
 
-            outl(GET_REG_ADDR(bus, dev, 0x2C), CONFIG_ADDRESS);
-            svenId = GET_SUBSYS_VENDOR_ID(inl(CONFIG_DATA));
-
-            venName = getVenName(venId);
-            devinf = getDevInfo(devId, venId);
-            clInf = getClassInfo(&cl);
-
-            printf(" %3x,%-2x  |  %04x  |  %04x  |   %02x,%02x,%02x  |  %2x  |  %4x  | ",
-                     bus,dev,    venId,  devId, cl.base,cl.sub,cl.interf, rev, svenId);
-            printf("%s %s %s %s %s %s",
-                    venName, devinf.name, devinf.desc, clInf.base, clInf.sub, clInf.interf);
-            puts("");
+                printf("%3x,%2x,%x | %04x | %04x| %02x,%02x,%02x | %2x| %4x | ",
+                         bus,dev,fu, venId, devId, cl.base,cl.sub,cl.interf, rev, svenId);
+                printf("%s: %s %s %s %s %s",
+                        venName, devinf.name, devinf.desc, clInf.base, clInf.sub, clInf.interf);
+                puts("");
+            }
         }
     }
 
